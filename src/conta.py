fa2baa2b17e764f8c0bee4e58402d2094af4a458
Pedro <pedro.moisesandrade@gmail.com>
2021-07-10 21:56:41 -0300

    Começando a construção.

---
+++ src/conta.py
@@ -0,0 +1,9 @@
+
+
+class Conta:
+
+    def __init__(self, numero, titular, saldo, limite=1000.0):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
