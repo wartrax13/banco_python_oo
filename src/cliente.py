724fa902a173fca52d3e811aa6c418b283b8b594
Pedro <pedro.moisesandrade@gmail.com>
2021-07-10 22:06:27 -0300

    Começando a construção 2

---
+++ src/cliente.py
@@ -0,0 +1,8 @@
+import conta
+
+class Cliente:
+    def __init__(self):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.cpf = cpf
+
--- src/conta.py
+++ src/conta.py
@@ -1,9 +1,22 @@
-
+import cliente
 
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite=1000.0):
         self.numero = numero
-        self.titular = titular
+        self.cliente = cliente
         self.saldo = saldo
         self.limite = limite
+
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+
+if __name__ == '__main__':
+    conta = Conta('123-4', 'João', 1000.0)
+    conta.deposita(100.0)
+    print(conta.numero)
+    print(conta.titular)
+    print(conta.saldo)
+    print(conta.limite)
